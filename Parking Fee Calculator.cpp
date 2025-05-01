6af4232bbd889cc429735dc265a20f19777acced
UncleH25 <oneilljarryd@gmail.com>
2025-05-01 18:27:51 +0200

    Declared intial variables.

--- Parking Fee Calculator.cpp
+++ Parking Fee Calculator.cpp
@@ -5,6 +5,12 @@ using namespace std;
 
 int main()
 {
+	// Constants
+    const double HOURLY_RATE = 10.00;
+	const double HALF_HOURLY_RATE = 5.00;
+
+	//Variables
+	int entryHour, entryMin, exitHour, exitMin;
     
 
     return 0;
